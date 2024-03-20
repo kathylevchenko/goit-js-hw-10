21c2b38be5d07dbeae943dce89e21783305861e2
kathylevchenko <145199639+kathylevchenko@users.noreply.github.com>
2024-03-20 14:10:04 +0200

    update 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delayInput = this.elements.delay;
+  const delay = parseInt(delayInput.value);
+
+  const stateInput = this.elements.state;
+  const state = stateInput.value;
+
+  let promise;
+
+  promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else if (state === 'rejected') {
+        reject(delay);
+      }
+    }, delay);
+  });
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+
+  event.currentTarget.reset();
+}
